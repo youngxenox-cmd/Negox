8484d739da0b7320243b59cafd05d9bc80eee638
Rahier hugo <rahier@MacBook-Pro-de-Rahier.local>
2026-04-10 14:32:47 +0200

    fix middleware typescript type

--- middleware.ts
+++ middleware.ts
@@ -26,7 +26,7 @@ export async function middleware(request: NextRequest) {
         getAll() {
           return request.cookies.getAll();
         },
-        setAll(cookiesToSet) {
+        setAll(cookiesToSet: { name: string; value: string; options?: any }[]) {
           cookiesToSet.forEach(({ name, value }) =>
             request.cookies.set(name, value)
           );
